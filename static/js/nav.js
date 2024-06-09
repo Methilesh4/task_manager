a3d050a6d7870f8977574d1b4e0a1d00a0f836ac
techtuff <techtuff4u@gmail.com>
2024-06-09 14:30:00 +0530

    index file with flask

---
+++ static/js/nav.js
@@ -0,0 +1,13 @@
+var btnContainer = document.getElementById("menu");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("menu__item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("menu__item--active");
+    current[0].className = current[0].className.replace(" menu__item--active", "");
+    this.className += " menu__item--active";
+  });
+}
